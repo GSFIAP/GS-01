40b0f8ca222d22c72b0a7fb31fe5f261d8b3a1da
Pedro Cunha <pedro55496969@gmail.com>
2025-06-03 20:11:12 -0300

    feat: Criação Reset CSS e Estilização do Site

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,6 @@ btnCor.addEventListener("click", function () {
     if (document.body.style.backgroundColor === "black") {
         document.body.style.backgroundColor = "white";
         document.body.style.color = "black";
-        btnCor.textContent = "Modo Escuro";
     } else {
         document.body.style.backgroundColor = "black";
         document.body.style.color = "white";
