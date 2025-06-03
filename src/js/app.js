990b037d2a837359313f2fd9a60e40481207b891
Pedro Cunha <pedro55496969@gmail.com>
2025-06-03 11:10:38 -0300

    feat: Adição de seções na landing page e funcionalidade de troca de cor

---
+++ src/js/app.js
@@ -0,0 +1,13 @@
+const btnCor = document.getElementById("btnCor");
+
+btnCor.addEventListener("click", function () {
+    if (document.body.style.backgroundColor === "black") {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+        btnCor.textContent = "Escuro";
+    } else {
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+        btnCor.textContent = "Claro";
+    }
+});
