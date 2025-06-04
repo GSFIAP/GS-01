a66125e8ad18b1bfc21b49bddb2b11bd1a8953da
Pedro Cunha <pedro55496969@gmail.com>
2025-06-03 21:37:16 -0300

    Validação Formulário

--- src/js/app.js
+++ src/js/app.js
@@ -10,3 +10,28 @@ btnCor.addEventListener("click", function () {
         btnCor.textContent = "Modo Claro";
     }
 });
+
+const inputNome = document.getElementById('nome');
+const inputEmail = document.getElementById('email');
+const inputMensagem = document.getElementById('mensagem');
+const btnEnviar = document.getElementById('btnEnviar');
+
+btnEnviar.addEventListener("click", function (event) {
+    event.preventDefault()
+
+    let nome = inputNome.value
+    let email = inputEmail.value
+    let mensagem = inputMensagem.value
+
+    if (nome === "" || email === "" ||  mensagem === "") {
+        
+        alert("TESTE")
+        // mensagemErro.textContent = "Preencha os campos vazios!"
+        // mensagemErro.style.color = "#ff0000"
+        // mensagemErro.classList.add('mensagemvermelha')
+        // mensagemErro.setAttribute("src", "link da imagem")
+        // mensagemErro.src = "link"
+        return
+
+        }
+    });
