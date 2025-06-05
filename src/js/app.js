e6e6d0641aebc451f1fcdf06b048c715cf3b983c
Pedro Cunha <pedro55496969@gmail.com>
2025-06-05 11:27:00 -0300

    feat: Icones Redes Sociais

--- src/js/app.js
+++ src/js/app.js
@@ -5,25 +5,47 @@ const inputEmail = document.getElementById('email');
 const inputEndereco = document.getElementById('endereco');
 const btnEnviar = document.getElementById('btnEnviar');
 const mensagemErro = document.getElementById('mensagemErro')
-const redesSociais = document.getElementById('redes-sociais')
+const redesSociais = document.getElementById('logo-redes-social')
 
 btnCorPreto.addEventListener("click", function () {
     html.classList.remove("modo-verde", "modo-azul") 
     html.classList.toggle("modo-preto");
+        redesSociais.innerHTML = `
+    <p>Venha conferir nossas redes sociais para mais atualizações!</p>
+       <div class="icones-redes">
+            <a href="https://api.whatsapp.com/send/?phone=5511976769012&text&type=phone_number&app_absent=0"><img  alt="Whatsapp" src="./src/assets/imgs/whatsapp_modo_preto.png"></a>
+            <a href="https://www.instagram.com/"><img alt="Instagram" src="./src/assets/imgs/instagram_modo_preto.png"></a>
+            <a href="https://www.linkedin.com/"><img alt="LinkedIn" src="./src/assets/imgs/linkedin_modo_preto.png"></a>
+        </div>
+    `
     
 });
 
 btnCorVerde.addEventListener("click", function () {
     html.classList.remove("modo-preto", "modo-azul")
     html.classList.toggle("modo-verde");
+    redesSociais.innerHTML = `
+    <p>Venha conferir nossas redes sociais para mais atualizações!</p>
+       <div class="icones-redes">
+            <a href="https://api.whatsapp.com/send/?phone=5511976769012&text&type=phone_number&app_absent=0"><img  alt="Whatsapp" src="./src/assets/imgs/whatsapp_modo_verde.png"></a>
+            <a href="https://www.instagram.com/"><img alt="Instagram" src="./src/assets/imgs/instagram_modo_verde.png"></a>
+            <a href="https://www.linkedin.com/"><img alt="LinkedIn" src="./src/assets/imgs/linkedin_modo_verde.png"></a>
+        </div>
+    `
 });
 
 btnCorAzul.addEventListener("click", function () {
     html.classList.remove("modo-verde", "modo-preto")
     html.classList.toggle("modo-azul");
-    // redesSociais.innerHTML = `
-    // <img id="whatsapp-modo-azul" src="./src/assets/imgs/whatsapp_modo_azul.png">
-    // `
+    redesSociais.innerHTML = `
+        <p>Venha conferir nossas redes sociais para mais atualizações!</p>
+       <div class="icones-redes">
+            <a href="https://api.whatsapp.com/send/?phone=5511976769012&text&type=phone_number&app_absent=0"><img  alt="Whatsapp" src="./src/assets/imgs/whatsapp_modo_azul.png"></a>
+            <a href="https://www.instagram.com/"><img alt="Instagram" src="./src/assets/imgs/instagram_modo_azul.png"></a>
+            <a href="https://www.linkedin.com/"><img alt="LinkedIn" src="./src/assets/imgs/linkedin_modo_azul.png"></a>
+        </div>
+    `
+
 });
 
 
